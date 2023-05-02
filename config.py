ed85195f16a0fb438b5770558cd118727c8b063e
Mohamed Atef Fahmy <muhamedfahmy74@yahoo.com>
2023-05-02 15:18:00 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+config = {
+     'bootstrap.servers': 'pkc-lzvrd.us-west4.gcp.confluent.cloud:9092',     
+     'security.protocol': 'SASL_SSL',
+     'sasl.mechanisms': 'PLAIN',
+     'sasl.username': 'P3N53QQVYS5ATDP2', 
+     'sasl.password': 'RoL5/jvfiPUk+npBsWjeciaxINrRzZsWvOYc4BrlS55Qvx0qEIgxg1W4LmZD0Wl8'}
